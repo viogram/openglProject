1b37e237fc9b4b2282c019960210d4858bed7408
26428 <26428@VIOGRAM>
2024-09-01 15:29:10 +0800

    延迟渲染

--- openglProject/HellowWindow.cpp
+++ openglProject/HellowWindow.cpp
@@ -20,6 +20,8 @@ float lastFrame = 0.0f; // 上一帧的时间
 float lastX = screenWidth/2, lastY = screenHeight/2; //初始鼠标位置
 bool firstMouse = false;  //是否第一次调用鼠标事件
 
+void renderQuad();
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -92,13 +94,77 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
     stbi_set_flip_vertically_on_load(true);
+
     //打开深度缓冲
     glEnable(GL_DEPTH_TEST);
+    const unsigned int NR_LIGHTS = 32;
+    std::vector<glm::vec3> lightPositions;
+    std::vector<glm::vec3> lightColors;
+    srand(13);
+    for (unsigned int i = 0; i < NR_LIGHTS; i++)
+    {
+        // calculate slightly random offsets
+        float xPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 3.0);
+        float yPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 4.0);
+        float zPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 3.0);
+        lightPositions.push_back(glm::vec3(xPos, yPos, zPos));
+        // also calculate random color
+        float rColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+        float gColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+        float bColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+        lightColors.push_back(glm::vec3(rColor, gColor, bColor));
+    }
 
+    //Shader
     Shader myShader("..\\shader\\vertex.txt", "..\\shader\\fragment.txt");
-
+    Shader debugShader("..\\shader\\debugVertex.txt", "..\\shader\\debugFragment.txt");
+    Shader lightingShader("..\\shader\\lightVertex.txt", "..\\shader\\lightFragment.txt");
     Model myModel("..\\model\\nanosuit\\nanosuit.obj");
     
+    //gBuffer
+    unsigned int gBuffer;
+    glGenFramebuffers(1, &gBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+    //创建纹理、法线、颜色+镜面缓冲
+    unsigned int gPosition, gNormal, AlbedoSpec;
+    glGenTextures(1, &gPosition);
+    glBindTexture(GL_TEXTURE_2D, gPosition);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
+
+    glGenTextures(1, &gNormal);
+    glBindTexture(GL_TEXTURE_2D, gNormal);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
+
+    glGenTextures(1, &AlbedoSpec);
+    glBindTexture(GL_TEXTURE_2D, AlbedoSpec);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, AlbedoSpec, 0);
+
+    GLuint attachments[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
+    glDrawBuffers(3, attachments);
+
+    //创建渲染缓冲对象作为深度缓冲
+    unsigned int depthRBO;
+    glGenRenderbuffers(1, &depthRBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, screenWidth, screenHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRBO);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    lightingShader.use();
+    lightingShader.setInt("gPosition", 0);
+    lightingShader.setInt("gNormal", 1);
+    lightingShader.setInt("gAlbedoSpec", 2);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -108,11 +174,11 @@ int main()
         lastFrame = currentTime;
         processInput(window);
 
-        //渲染指令
+        //渲染gBuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glm::vec3 lightPos(1.2f, 1.0f, 1.0f);
         glm::mat4 model=glm::mat4(1.0f);
         
         glm::mat4 view = glm::mat4(1.0f);
@@ -123,19 +189,41 @@ int main()
         
         myShader.use();
         myShader.setFloat("material.shininess", 32);
-        myShader.setVec3("light.position", lightPos);
-        myShader.setVec3("light.ambient", 1.0f, 1.0f, 1.0f);
-        myShader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
-        myShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        myShader.setFloat("light.constant", 1.0f);
-        myShader.setFloat("light.linear", 0.09f);
-        myShader.setFloat("light.quadratic", 0.032f);
-        myShader.setVec3("viewPos", camera.Position);
         myShader.setMatirx4f("model", model);
         myShader.setMatirx4f("view", view);
         myShader.setMatirx4f("project", project);
         myShader.setMatirx3f("toWorldNormal", glm::mat3(glm::transpose(glm::inverse(model))));
         myModel.Draw(myShader);
+
+        //渲染屏幕
+        //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //debugShader.use();
+        //glActiveTexture(GL_TEXTURE0);
+        //glBindTexture(GL_TEXTURE_2D, AlbedoSpec);
+        //renderQuad();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        lightingShader.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, gPosition);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, gNormal);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, AlbedoSpec);
+        for (unsigned int i = 0; i < lightPositions.size(); i++)
+        {
+            lightingShader.setVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
+            lightingShader.setVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
+            // update attenuation parameters and calculate radius
+            const float linear = 0.7f;
+            const float quadratic = 1.8f;
+            lightingShader.setFloat("lights[" + std::to_string(i) + "].Linear", linear);
+            lightingShader.setFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
+        }
+        lightingShader.setVec3("viewPos", camera.Position);
+        renderQuad();
 
         //检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
@@ -144,4 +232,33 @@ int main()
     glDeleteProgram(myShader.ID);
     glfwTerminate();
     return 0;
+}
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
 }
