d6f9bbaa9f9ed0c81aabbe504a0287958fcf33f9
26428 <26428@VIOGRAM>
2024-09-02 21:02:03 +0800

    Revert "IBL 镜面反射 漫反射"
    This reverts commit f8ebdedf3a654aa7ab0d25c55219362c2f3c9a37.

--- openglProject/HellowWindow.cpp
+++ openglProject/HellowWindow.cpp
@@ -253,7 +253,6 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
 
-
     if (firstMouse)
     {
         lastX = xpos;
